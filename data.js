a198e2d7b1fce6bcdb16d943c923165e73750e79
M4G1C-maker <59521448+M4G1C-maker@users.noreply.github.com>
2022-01-31 10:47:14 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,134 @@
+export const data = [
+  {
+    id: 1,
+    name: 'design',
+    title: 'Design',
+    desc: 'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.',
+    video: 'one.mp4',
+    photo: 'asdasd.png',
+    longDesc:
+      'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library. Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.',
+    images: [
+      {
+        id: 1,
+        url: 'https://images.pexels.com/photos/962000/pexels-photo-962000.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        local: 'user1.jpeg',
+      },
+      {
+        id: 2,
+        url: 'https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        local: 'user2.jpeg',
+      },
+      {
+        id: 3,
+        url: 'https://images.pexels.com/photos/3593865/pexels-photo-3593865.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        local: 'user3.jpeg',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'development',
+    title: 'Development',
+    desc: 'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.',
+    photo: 'dev.jpeg',
+    longDesc:
+      'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library. Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.',
+    images: [
+      {
+        id: 1,
+        url: 'https://images.pexels.com/photos/9965121/pexels-photo-9965121.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        local: 'user1.jpeg',
+      },
+      {
+        id: 2,
+        url: 'https://images.pexels.com/photos/9716799/pexels-photo-9716799.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        local: 'user2.jpeg',
+      },
+      {
+        id: 3,
+        url: 'https://images.pexels.com/photos/9395308/pexels-photo-9395308.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        local: 'user3.jpeg',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'production',
+    title: 'Production',
+    desc: 'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.',
+    photo: 'production.jpeg',
+    longDesc:
+      'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library. Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.',
+    images: [
+      {
+        id: 1,
+        url: 'https://images.pexels.com/photos/9299384/pexels-photo-9299384.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        local: 'user1.jpeg',
+      },
+      {
+        id: 2,
+        url: 'https://images.pexels.com/photos/9100089/pexels-photo-9100089.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        local: 'user2.jpeg',
+      },
+      {
+        id: 3,
+        url: 'https://images.pexels.com/photos/9714732/pexels-photo-9714732.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        local: 'user3.jpeg',
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'photography',
+    title: 'Photography',
+    desc: 'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.',
+    photo: 'photography.jpeg',
+    longDesc:
+      'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library. Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.',
+    images: [
+      {
+        id: 1,
+        url: 'https://images.pexels.com/photos/962000/pexels-photo-962000.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        local: 'user1.jpeg',
+      },
+      {
+        id: 2,
+        url: 'https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        local: 'user2.jpeg',
+      },
+      {
+        id: 3,
+        url: 'https://images.pexels.com/photos/3593865/pexels-photo-3593865.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        local: 'user3.jpeg',
+      },
+    ],
+  },
+];
+
+export const users = [
+  {
+    id: 1,
+    logo: 'yahoo.png',
+    avatar: 'user1.jpeg',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Utconsectetur veniam fugiat labore quaerat possimus laudantium.',
+    name: 'Harold Brown',
+    title: 'Executive Manager',
+  },
+  {
+    id: 2,
+    logo: 'google.png',
+    avatar: 'user2.jpeg',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Utconsectetur veniam fugiat labore quaerat possimus laudantium.',
+    name: 'Anna Taylor',
+    title: 'HR Manager',
+  },
+  {
+    id: 3,
+    logo: 'facebook.png',
+    avatar: 'user3.jpeg',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Utconsectetur veniam fugiat labore quaerat possimus laudantium.',
+    name: 'James Garry',
+    title: 'UI Designer',
+  },
+];
